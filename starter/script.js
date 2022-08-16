25d58ac9ac3bb3a666ed88774d7db1c308b0faaa
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2022-08-16 12:29:15 +0100

    commit

--- starter/script.js
+++ starter/script.js
@@ -574,3 +574,17 @@ const { deposit, withdrawal } = accounts
   );
 
 console.log(deposit, withdrawal);
+
+////convert titlecase
+const convertTitleCase = function (title) {
+  const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCas = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => word[0].toUpperCase() + word.slice(1));
+  return titleCas;
+};
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG  title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
